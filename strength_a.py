f9569a249b0f17b525ee5e5d41a24f21f978e628
p-aufdencamp <p.aufdencamp@gmail.com>
2024-01-22 21:40:45 -0800

    Added back in Squat & Deadlift Combined front plank & chest press into one isometric pushup

--- strength_a.py
+++ strength_a.py
@@ -1,5 +1,8 @@
 # Author: Phil Aufdencamp
 
+# (X) Add back in Squat & Deadlift
+# (X) Combine Front Plank and Chest Press into one Isometric Pushup
+
 # This file will store the list of objects which makes up the banded iso
 # routine
 
@@ -14,18 +17,22 @@ from exercise import Time_Based
 def get_strength_a_routine(settings):
     
     if settings['selected_user'] == "p":
-        routine = [Time_Based("Plank on Elbows",0,45), #Banded iso routine
-          Reps_Based("Lying Side Leg Raise, Left","Bodyweight",10),
-          Reps_Based("Lying Side Leg Leg, Right","Bodyweight",10),
-          Time_Based("Banded Bridge","Medium Band",45),
-          Time_Based("Banded Narrow Knees","Light Band",45),
-          Time_Based("Banded Back Neck Hold","Light Band",45),
-          Time_Based("Banded Front Neck Hold","Light Band",45),
-          Time_Based("Banded Front Raise","Light Band",45),
-          Time_Based("Banded Chest Press","Medium Heavy Band",45),
-          Time_Based("Banded Row","Medium Heavy Band",45),
-          Time_Based("Left Plank","Body Weight",20),
-          Time_Based("Right Plank","Body Weight",20)]
+        # Banded Iso Routine
+        routine = [
+        Reps_Based("Lying Side Leg Raise, Left","Bodyweight",10),
+        Reps_Based("Lying Side Leg Leg, Right","Bodyweight",10),
+        Time_Based("Banded Bridge","Medium Band",45),
+        Time_Based("Banded Narrow Knees","Light Band",45),
+        Time_Based("Banded Back Neck","Light Band",45),
+        Time_Based("Banded Back Neck Hold","Light Band",45),
+        Time_Based("Banded Front Raise","Light Band",45),
+        Time_Based("Banded Deadlift","Medium Heavy Band",45),
+        Time_Based("Isometric Pushup","Light Band",45),
+        Time_Based("Banded Squat","Light Band",45),
+        Time_Based("Banded Row","Medium Heavy Band",45),
+        Time_Based("Left Plank","Body Weight",45),
+        Time_Based("Right Plank","Body Weight",45)]
+        
     elif settings['selected_user'] == "c":
         routine = [Reps_Based("Pushups",0,15),#Set 1
             Reps_Based("Military Press","25lbs ea",10),
