035df9124dd2cebd9244ba1267a0aa2299fcfe4c
p-aufdencamp <p.aufdencamp@gmail.com>
2024-03-12 08:22:48 -0700

    New Routine:  SL HR leg straight focus on keeping ankle outwards 2x20 SL HR knee bent focus on keeping ankle outwards 2x20 Double leg Decline squat with weight 4x12 SL squat heel elevated 3x10 DL squat jumps with heel elevated 3x20s 6" lateral step down with arch lift 3x12

--- rehab_strength.py
+++ rehab_strength.py
@@ -12,34 +12,43 @@ from exercise import Interval
 from exercise import Reps_Based
 from exercise import Time_Based
 
+
 def get_rehab_strength_routine(settings):
     
     if settings['selected_user'] == "p":
         routine = [
         #Set -1
-        Reps_Based("Single Leg Heel Raise",0,20),
+        Reps_Based("Single Leg Heel Raise, straight leg, keep ankle out",0,20),
         Time_Based("Rest",0,30),
+        Reps_Based("Single Leg Heel Raise, knee Bent, keep ankle out",0,20),
+        Time_Based("Rest",0,30),
+
         #Set 0
-        Reps_Based("Single Leg Heel Raise",0,20),
+        Reps_Based("Single Leg Heel Raise, keep ankle out",0,20),
+        Time_Based("Rest",0,30),
+        Reps_Based("Single Leg Heel Raise, knee Bent, keep ankle out",0,20),
         Time_Based("Rest",0,30),
         
         # Set 1
-        Reps_Based("Hands on Chair Plank into Bike Pose",0,8),
-        Time_Based("Double Leg Squat Jumps",0,30),
-        Time_Based("Bear Plank",0,30),
-        Time_Based("Straight Leg Tall Hop",0,20),
+        Reps_Based("Double Leg Decline Squat w/ weight",30,12),
+        Reps_Based("Single Leg Squat Heel Elevated",0,10),
+        Time_Based("Double Leg Squat Jumps",0,20),
+        Reps_Based("6in Lateral Step down w/ Arch Lift",0,12),
 
         #Set 2
-        Reps_Based("Hands on Chair Plank into Bike Pose",0,8),
-        Time_Based("Double Leg Squat Jumps",0,30),
-        Time_Based("Bear Plank",0,30),
-        Time_Based("Straight Leg Tall Hop",0,20),
+        Reps_Based("Double Leg Decline Squat w/ weight",30,12),
+        Reps_Based("Single Leg Squat Heel Elevated",0,10),
+        Time_Based("Double Leg Squat Jumps",0,20),
+        Reps_Based("6in Lateral Step down w/ Arch Lift",0,12),
         
         #Set 3
-        Reps_Based("Hands on Chair Plank into Bike Pose",0,8),
-        Time_Based("Double Leg Squat Jumps",0,30),
-        Time_Based("Bear Plank",0,30),
-        Time_Based("Straight Leg Tall Hop",0,20)
+        Reps_Based("Double Leg Decline Squat w/ weight",30,12),
+        Reps_Based("Single Leg Squat Heel Elevated",0,10),
+        Time_Based("Double Leg Squat Jumps",0,20),
+        Reps_Based("6in Lateral Step down w/ Arch Lift",0,12),
+
+        # Bonus Set
+        Reps_Based("Double Leg Decline Squat w/ weight",30,12)
         ]
     elif settings['selected_user'] == "c":
         routine = [
