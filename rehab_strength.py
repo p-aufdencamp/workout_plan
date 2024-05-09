000ce53124b780b3dfeda58c0b4831d2c8b08f54
p-aufencamp <p.aufdencamp@gmail.com>
2024-05-08 23:16:39 -0700

    Updated Rehab Strength Routine

--- rehab_strength.py
+++ rehab_strength.py
@@ -22,42 +22,21 @@ def get_rehab_strength_routine(settings):
     
     if settings['selected_user'] == "p":
         routine = [
-        # Skater hops 2x30s
-        Time_Based("Skater Hops",0,30),
-        Time_Based("Rest",0,30),
-        Time_Based("Skater Hops",0,20),
-        Time_Based("Rest",0,30),
-
-        #Sidestepping with band 3x30s
-        Time_Based("Side Stepping w/ Band",0,30),
-        Time_Based("Rest",0,30),
-        Time_Based("Side Stepping w/ Band",0,30),
-        Time_Based("Rest",0,30),
-        Time_Based("Side Stepping w/ Band",0,30),
-        Time_Based("Rest",0,30),
-        
-        #Bear plank with chest taps 3x5 on each side
-        Reps_Based("Bear Plank w/ Alternating Chest Taps",0,5),
-        Time_Based("Rest",0,30),
-        Reps_Based("Bear Plank w/ Alternating Chest Taps",0,5),
-        Time_Based("Rest",0,30),
-        Reps_Based("Bear Plank w/ Alternating Chest Taps",0,5),
-        Time_Based("Rest",0,30),
 
         #SL hopping with assistance 3x15s
-        Time_Based("Single Leg Hop w/ Assistance",0,15),
+        Time_Based("Single Leg Hop",0,20),
         Time_Based("Rest",0,30),
-        Time_Based("Single Leg Hop w/ Assistance",0,15),
+        Time_Based("Single Leg Hop",0,20),
         Time_Based("Rest",0,30),
-        Time_Based("Single Leg Hop w/ Assistance",0,15),
+        Time_Based("Single Leg Hop",0,20),
         Time_Based("Rest",0,30),
         
-        #SL squat mini bobs 3x20
-        Reps_Based("Single Leg Squat Mini Bobs",0,20),
+        #Lateral Lunge 
+        Reps_Based("Lateral Lunge Right",0,8),
         Time_Based("Rest",0,30),
-        Reps_Based("Single Leg Squat Mini Bobs",0,20),
+        Reps_Based("Lateral Lunge Right",0,8),
         Time_Based("Rest",0,30),
-        Reps_Based("Single Leg Squat Mini Bobs",0,20),
+        Reps_Based("Lateral Lunge Right",0,8),
         Time_Based("Rest",0,30)
         ]
     elif settings['selected_user'] == "c":
